8ef1c1d93a5cd5e7468d665234a12e1a8e3e810b
Georg Wölflein <georgw7777@gmail.com>
2018-04-07 19:34:33 +0100

    Fix typo

--- app/js/app.js
+++ app/js/app.js
@@ -21,7 +21,7 @@ class App {
     let y = -event.beta;
     if (Math.abs(x) <= 5) x = 0;
     if (Math.abs(y) <= 5) y = 0;
-    let obj = { type: 'move', x: x, y: y };
+    let obj = { type: 'move', x, y };
     this.ws.send(JSON.stringify(obj));
   }
 
@@ -29,7 +29,7 @@ class App {
     let x = event.accelerationIncludingGravity.x;
     let y = -event.accelerationIncludingGravity.y;
     if (Math.abs(x) >= 15 || Math.abs(x) >= 15) {
-      let obj = { type: 'shoot', x: x, y: y };
+      let obj = { type: 'shot', x, y };
       this.ws.send(JSON.stringify(obj));
       log(JSON.stringify(obj));
     }
