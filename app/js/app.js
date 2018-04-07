438776b705650a65808225706d0428d3e1de9ce3
Georg Wölflein <georgw7777@gmail.com>
2018-04-07 17:42:26 +0100

    Allow completely level positioning

--- app/js/app.js
+++ app/js/app.js
@@ -9,15 +9,16 @@ class App {
   init() {
     this.ws = new WebSocket(webSocketUrl);
     console.log(this.ws);
-    window.addEventListener('deviceorientation', (e)=>{this.handleOrientation(e)}, true);
+    window.addEventListener('deviceorientation', (e) => { this.handleOrientation(e) }, true);
     log('initialized');
   }
 
   handleOrientation(event) {
-
     var x = event.gamma;
-    var y = event.beta;
-    var obj = { type: 'move', x:x, y:y }
+    var y = -event.beta;
+    if (Math.abs(x) <= 5) x = 0;
+    if (Math.abs(y) <= 5) y = 0;
+    var obj = { type: 'move', x: x, y: y }
     this.ws.send(JSON.stringify(obj));
   }
 }
