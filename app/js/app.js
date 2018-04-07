584facc55bf663c4870901656654be1202b29cc4
Georg Wölflein <georgw7777@gmail.com>
2018-04-07 18:48:37 +0100

    Fix errors

--- app/js/app.js
+++ app/js/app.js
@@ -7,28 +7,28 @@ let webSocketUrl = 'wss://js321.host.cs.st-andrews.ac.uk/nitefort/user';
 class App {
 
   init() {
-    window.addEventListener('devicemotion', this.handleMotion);
-    /*
     this.ws = new WebSocket(webSocketUrl);
     this.ws.onopen = () => {
       window.addEventListener('deviceorientation', (e) => this.handleOrientation(e), true);
       window.addEventListener('devicemotion', (e) => this.handleMotion(e), true);
     };
     this.ws.onmessage = (e) => console.log(e.data);
-    log('initialized');*/
+    log('initialized');
   }
 
   handleOrientation(event) {
-    var x = event.gamma;
-    var y = -event.beta;
+    let x = event.gamma;
+    let y = -event.beta;
     if (Math.abs(x) <= 5) x = 0;
     if (Math.abs(y) <= 5) y = 0;
-    var obj = { type: 'move', x: x, y: y }
+    let obj = { type: 'move', x: x, y: y }
     this.ws.send(JSON.stringify(obj));
   }
 
   handleMotion(event) {
-    log(event.accelerationIncludingGravity.x);
+    let x = event.accelerationIncludingGravity.x;
+    let y = event.accelerationIncludingGravity.y;
+    log(JSON.stringify({ x, y }));
   }
 }
 
