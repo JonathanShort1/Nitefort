3e29827eb86b4098a85d9cd82395165bea9927c6
Georg Wölflein <georgw7777@gmail.com>
2018-04-07 18:19:10 +0100

    Add tests

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,5 @@
 function log(data) {
-  $('body').prepend('<p>' + data + '</p>');
+  $('body').append('<pre>' + data + '</pre>');
 }
 
 let webSocketUrl = 'wss://js321.host.cs.st-andrews.ac.uk/nitefort/user';
@@ -7,10 +7,15 @@ let webSocketUrl = 'wss://js321.host.cs.st-andrews.ac.uk/nitefort/user';
 class App {
 
   init() {
+    window.addEventListener('devicemotion', this.handleMotion);
+    /*
     this.ws = new WebSocket(webSocketUrl);
-    this.ws.onopen = () => window.addEventListener('deviceorientation', (e) => this.handleOrientation(e), true);
+    this.ws.onopen = () => {
+      window.addEventListener('deviceorientation', (e) => this.handleOrientation(e), true);
+      window.addEventListener('devicemotion', (e) => this.handleMotion(e), true);
+    };
     this.ws.onmessage = (e) => console.log(e.data);
-    log('initialized');
+    log('initialized');*/
   }
 
   handleOrientation(event) {
@@ -20,6 +25,12 @@ class App {
     if (Math.abs(y) <= 5) y = 0;
     var obj = { type: 'move', x: x, y: y }
     this.ws.send(JSON.stringify(obj));
+  }
+
+  handleMotion(event) {
+    var x = event.acceleration.x;
+    var y = event.acceleration.y;
+    log(JSON.stringify({ x, y }));
   }
 }
 
